94ae961f9a991ce8274ee09908e9a9287f1c96c7
Bale Harsha Vardhan <128736919+harshavardhanbale@users.noreply.github.com>
2023-11-28 01:35:46 +0530

    Update harsha.py

--- harsha.py
+++ harsha.py
@@ -16,7 +16,7 @@ default_args={
     'depends_on_past':False,
     'retries':0
 }
-#hellohai
+#hellohaibye
 dag=DAG(dag_id='DAG-1',default_args=default_args,catchup=False,schedule_interval='@once')
 #hai
 hello_operator = PythonOperator(task_id='hello_task', python_callable=TEST, dag=dag)
